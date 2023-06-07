ce359078b2243bb3fb8827df5607f2c343e31bea
Matthew Woodhall <mattie32@hotmail.co.uk>
2023-06-07 21:25:22 +0100

    commit

---
+++ script.js
@@ -0,0 +1,38 @@
+document.addEventListener("DOMContentLoaded", function () {
+//mobile nav menu variables
+  const hamburgerIcon = document.querySelector(".hamburger-icon");
+  const navMenu = document.querySelector(".nav-menu");
+  const overlay = document.querySelector(".overlay");
+  navMenu.style.display = "none";
+
+  // Toggle the menu visibility on click - mobile view
+  hamburgerIcon.addEventListener("click", function () {
+    navMenu.classList.toggle("open");
+    overlay.classList.toggle("open");
+    if (navMenu.classList.contains("open")) {
+      navMenu.style.display = "block";
+    } else {
+      navMenu.style.display = "none";
+    }
+  });
+
+ 
+  //feature to change the button text from "Personalised video" to "book a test drive" as per the designs
+  const topFeatureBtn = document.querySelector(".personalised-video-btn");
+  if(checkBreakpoint() == "mobile"){
+    topFeatureBtn.innerHTML = "Book a test drive";
+  }else{
+    topFeatureBtn.innerHTML = "Request a Personalised Video";
+  }
+  
+  function checkBreakpoint() {
+    const windowWidth = window.innerWidth || document.documentElement.clientWidth;
+    const breakpoint = 768;
+  
+    if (windowWidth < breakpoint) {
+      return "mobile"
+    } else {
+      return "desktop"
+    }
+  }
+});
